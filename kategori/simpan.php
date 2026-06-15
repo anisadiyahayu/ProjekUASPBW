0cbbe5c254947f54fc3a9cf7bc2c37402f16019a
anisadiyahayu <anisadiyahayu24@gmail.com>
2026-06-15 09:46:35 +0700

    Menambahkan fitur CRUD kategori

---
+++ kategori/simpan.php
@@ -0,0 +1,17 @@
+<?php
+
+include("../koneksi.php");
+
+$id = $_POST['id'];
+$nama = $_POST['nama'];
+$deskripsi = $_POST['deskripsi'];
+
+mysqli_query(
+    $koneksi,
+    "INSERT INTO categories
+    (id,nama,deskripsi)
+    VALUES
+    ('$id','$nama','$deskripsi')"
+);
+
+header("Location:index.php");
