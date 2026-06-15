77c086a02ea11e9efd4e800ab7bb8a9960cbf872
Qyyuuu <rifqykurniawan42@gmail.com>
2026-06-16 06:19:21 +0700

    UPDATE

--- inventaris lab/admin/data_barang.php
+++ inventaris lab/admin/data_barang.php
@@ -2,6 +2,7 @@
 session_start();
 include '../koneksi.php';
 include 'fungsi_stok.php';
+include "../include/header.php";
 
 if (!isset($_SESSION['user_id'])) {
     header("Location: ../login.php");
--- inventaris lab/admin/index.php
+++ inventaris lab/admin/index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-
+include "../include/header.php";
 if (!isset($_SESSION['user_id'])) {
     header("Location: ../login.php");
     exit;
