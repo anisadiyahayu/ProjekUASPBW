d0bf0b8d2ceb6936eb20f9ef53409fd2b8f0d1e7
anisadiyahayu <anisadiyahayu24@gmail.com>
2026-06-16 18:00:32 +0700

    perbarui

---
+++ koneksi.php
@@ -0,0 +1,22 @@
+<?php
+$conn = new mysqli("localhost", "root", '', "db_invertaris_lab");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "simlab";
+
+$conn = mysqli_connect(
+    $host,
+    $username,
+    $password,
+    $database
+);
+
+if (!$conn) {
+    die("Koneksi gagal : " . mysqli_connect_error());
+}
+}?>
